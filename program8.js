3f80dc18f63ccde6a2e57ca71b6e5ab5a3bb596a
Anisul Islam <anisul.islam@tuni.fi>
2020-12-12 17:42:49 +0200

    Area of various shapes using arithmetic operator

--- program8.js
+++ program8.js
@@ -1,7 +1,14 @@
 // Area of various shapes
 
 //Area of triangel
+//getting user input for base
 var base = parseFloat(prompt("Enter Base = "));
+
+//getting user input for height
 var height = parseFloat(prompt("Enter Height = "));
+
+//calculating area 
 var area = 0.5 * base * height;
+
+//printing area
 document.write("Area of triangle = " + area);
