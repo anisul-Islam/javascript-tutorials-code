4d1db96e8b9aa8f2a5b827f02a79c9c589817477
Anisul Islam <anisul.islam@tuni.fi>
2020-12-12 15:49:37 +0200

    js output functions

--- program1.js
+++ program1.js
@@ -4,6 +4,7 @@
 
 document.write("Welcome to JS Program");
 document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
 
 
 console.log("Welcome to JS Program");
