2f3e5622fb99e3be7d59b9fff649f2ae0f33685b
Anisul Islam <anisul.islam@tuni.fi>
2020-12-12 16:00:55 +0200

    keywork, data type, comment in js

--- program2.js
+++ program2.js
@@ -1,3 +1,5 @@
+//you can run this program by using the command =>   node program2.js
+
 // keywords -> reserved words for specific reasons. example -> for,while, do while, if, else, else if, break, try,catch, function, var, const, let etc.
 // data type -> string, number (int,float,double), boolean, object. use typeof() to check the data type
 // comment -> single line comment, double line comment
@@ -6,3 +8,4 @@ console.log(typeof (123));
 console.log(typeof (123.5));
 console.log(typeof ("123"));
 console.log(typeof (true));
+console.log(typeof (false));
