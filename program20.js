a3036a8f319651eed6358d2a1fa0d70fd3aa4040
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 08:58:09 +0200

    Array  example

--- program20.js
+++ program20.js
@@ -1,10 +1,15 @@
+// Array program
+
 // Array -> is a collection of variables
+// Array VS Object -> arrays use numbered indexes. but, objects use named indexes.
 
 // creating an array with 20 variables
 var names = new Array(20);
+
 //array index always start with 0
 names[0] = "Anisul";
 names[1] = "Sonali";
+
 //printing an array elements
 console.log(names[1]);
 
@@ -12,6 +17,7 @@ console.log(names[1]);
 var students = ["Anisul", "Sonali", "Priya", "Numan"];
 console.log("students = " + students);
 console.log("studnet at 2 index : " + students[2]);
+
 //finding the length of an array
 console.log("Length of student array : " + students.length)
 
