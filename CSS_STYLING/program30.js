3559a9b686d4dda39ecf5fc7942336e35dd1a4aa
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 11:34:22 +0200

    Adding & removing css style in js

---
+++ CSS_STYLING/program30.js
@@ -0,0 +1,12 @@
+// Adding & removing css style in js
+
+var h1 = document.querySelector("h1");
+h1.innerHTML = "hiiii"
+console.log(h1)
+
+//add style
+h1.classList.add('heading-style')
+
+
+//removing style
+// h1.classList.remove('heading-style')
