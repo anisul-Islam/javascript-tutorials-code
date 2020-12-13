618f621e7982eb439665b87bdb6cbefc94c5dff3
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 11:16:44 +0200

    Create, remove, add html element

--- program29.js
+++ program29.js
@@ -1,17 +1,25 @@
-// Error handling -> try , catch, finally , throw
-// try ... catch handle run time errors (program which is runable), but not syntax error
-// Error object
-// The finally statement -> execute code, after try and catch 
+//Create, remove, add html element
 
-try{
-    // code test
-    alert("Hi Everyone");
-    alert(x);
-    alert("Not gonna work");   
+// creating html element and adding to a div
+var firstDiv = document.querySelector("#first-div");
+console.log(firstDiv)
 
-}catch(err){
-    console.log(err);
-}finally{
-    alert("Bye Everyone");
-}
+var heading3 = document.createElement("h3");
+var text = document.createTextNode("This is heading 3");
+heading3.appendChild(text);
+firstDiv.appendChild(heading3);
 
+var heading5 = document.createElement("h5");
+var text = document.createTextNode("This is heading 5");
+heading5.appendChild(text);
+firstDiv.appendChild(heading5);
+
+// remove element
+firstDiv.removeChild(heading5);
+
+//adding element before
+var heading6 = document.createElement("h6");
+var text = document.createTextNode("This is heading 6");
+heading6.appendChild(text);
+firstDiv.insertBefore(heading6, heading3);
+// firstDiv.insertAfter(heading6, heading3);
