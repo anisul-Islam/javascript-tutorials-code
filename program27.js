7030b44e6fe6c846f4d5f1cd22ad9bc355475388
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 10:40:40 +0200

    How to find HTML Elements

--- program27.js
+++ program27.js
@@ -1,27 +1,44 @@
-// for vs foreach
+// DOM -> Document Object Model
+// Using DOM We can Find / get, change, add, or delete HTML elements.
 
-// var numbers = [10,20,30,40];
-// for(var x=0; x<numbers.length; x++)
-// {
-//     console.log(numbers[x]);
-// }
+// How to find HTML Elements 
+// document.getElementById()
+// document.getElementsByTagName()
+// document.getElementsByClassName()
+// document.querySelector()
 
-// var numbers = [10,20,30,40];
-// numbers.forEach(function(x){
-//     console.log(x)
-// })
 
-// var numbers = [10,20,30,40];
-// var squareNumbers = [];
-// numbers.forEach(function(x){
-//     squareNumbers.push(x*x);
-// })
-// console.log(squareNumbers)
+// finding element by ID
+var h2 = document.getElementById("heading2");
+console.log(h2)
 
-var numbers = [10,20,30,40];
-console.log(numbers)
-numbers.forEach(function(x,index,arr){
-    arr[index] = x+5;
-})
-console.log(numbers)
+
+// finding element by tagName
+var h1 = document.getElementsByTagName("h1")[0];
+console.log(h1)
+
+
+// finding element by tagName
+var p = document.getElementsByClassName("para")[0];
+console.log(p)
+
+
+// finding element by querySelector
+var q1 = document.querySelector("#heading2");
+console.log(q1)
+
+//it will get the first element of the h1 elements
+var q2 = document.querySelector("h1");
+console.log(q2)
+
+//it will get the first element of the para class
+var q3 = document.querySelector(".para");
+console.log(q3)
+
+var list = document.querySelector(".my-div li");
+console.log(list)
+
+// finding element by querySelectorAll
+var q4 = document.querySelectorAll(".para");
+console.log(q4)
 
