66d9d31c1fdd354b07413370e5c9aff45f02ea86
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 11:40:41 +0200

    How to create, add, remove HTML Elements in JS

--- program28.js
+++ program28.js
@@ -7,7 +7,7 @@
 // finding element by ID
 var h2 = document.getElementById("heading2");
 
-// changine elements text, id
+// changing elements text, id
 h2.innerHTML = "I love Bangladesh";
 h2.id = "heading2-new";
 console.log(h2);
@@ -17,5 +17,9 @@ console.log(a)
 a.textContent="visit my website";
 a.href="http://www.studywithanis.com"
 
+// changing style of element
+a.style.textDecoration = "none";
+a.style.color = "green";
+a.style.fontSize = "2rem";
 
 
