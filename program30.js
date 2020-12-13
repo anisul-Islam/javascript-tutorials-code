4c48de96a8dd9db9742e97a500fa629cd58b09d4
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 11:27:56 +0200

    Adding & removing css style in js

--- program30.js
+++ program30.js
@@ -1,21 +1,17 @@
-// Error handling - try, catch, finally
-// The throw statement -> create custom errors.
+// Adding & removing css style in js
 
-document.querySelector("#checkButton").addEventListener("click",function(){
-   
-    var num = document.querySelector("#numTextfield").value;
-    
-    try{
-        if(num < 5)
-        {
-            throw "input is too low"
-        } else if(num > 10)
-        {
-            throw "input is too high"
-        }
+// step 1 : create index.html and add a h1 element
+// setp 2 : create a style.css file and add some style inside a class called heading-style
+// step 3 : create a js file and use the code from this file
+// step 4 : link css and js file to your index.html file
 
-    }catch(err){
-        console.log(err);
-    }
 
-})
+var h1 = document.querySelector("h1");
+console.log(h1)
+
+//add style
+h1.classList.add('heading-style')
+
+
+//removing style
+h1.classList.remove('heading-style')
