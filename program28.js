fa2301970a15f6d1380d235ef06bf33fdecbb4d0
Anisul Islam <anisul.islam@tuni.fi>
2020-12-13 10:52:19 +0200

    How to change HTML Elements

--- program28.js
+++ program28.js
@@ -1,14 +1,24 @@
-// forEach , map, filter
+// DOM -> Document Object Model
+// Using DOM We can Find / get, change, add, or delete HTML elements.
 
-var numbers = [22,31,4,5,35,26,78]
-var squareNumbers = numbers.map(function(x){
-   return x*x;
-})
-console.log(squareNumbers)
+// How to change HTML Elements  (collected from W3School website)
+// element.innerHTML =  new html content ->	Change the inner HTML of an element
+// element.attribute = new value 	-> Change the attribute value of an HTML element
+// element.style.property = new style ->	Change the style of an HTML element
 
 
-var numbers = [22,31,4,5,35,26,78]
-var newNumbers = numbers.filter(function(x){
-   return x>10;
-})
-console.log(newNumbers)
+// finding element by ID
+var h2 = document.getElementById("heading2");
+
+// changine elements text, id
+h2.innerHTML = "I love Bangladesh";
+h2.id = "heading2-new";
+console.log(h2);
+
+var a = document.querySelector("a");
+console.log(a)
+a.textContent="visit my website";
+a.href="http://www.studywithanis.com"
+
+
+
