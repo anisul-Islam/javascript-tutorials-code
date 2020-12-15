c473160178d1074b27f57389bad5fc56d6e9e637
Anisul Islam <anisul.islam@tuni.fi>
2020-12-15 21:17:07 +0200

    default and rest parameter

--- ES6 FEATURES/1_ES6_SYNTAX/1.4_DEFAULT_AND_REST_PARAMETER.js
+++ ES6 FEATURES/1_ES6_SYNTAX/1.4_DEFAULT_AND_REST_PARAMETER.js
@@ -1,18 +1,17 @@
+"use strict"
+
 //example of default parameter
 function message(text='Hello Everyone') {
     console.log(text);
 }
-
 message(); 
 message("love js es6");
 
-//example of rest parameter
-function sum(x, y, ...z){
-    let total = 0;
-    for (const num of z) {
-        total += num;
-    }
-    console.log(x, y, z, total)
-}
 
-sum(1,2,3,4,5,6);
+
+//example of rest parameter
+function printNumbers(x,y, ...z){
+    console.log(`x = ${x}, y = ${y}, z= ${z}`)
+}
+printNumbers(10,20,30,40,50);
+
