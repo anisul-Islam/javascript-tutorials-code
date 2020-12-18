917eb0ac4e8e26bf1fdde8c90315b9d6c8137056
Anisul Islam <anisul.islam@tuni.fi>
2020-12-19 00:34:29 +0200

    for...of and for...in loop

--- ES6 FEATURES/1_ES6_SYNTAX/1.7_FOROF_FORIN.js
+++ ES6 FEATURES/1_ES6_SYNTAX/1.7_FOROF_FORIN.js
@@ -1,6 +1,6 @@
 // for..of loop can iterate over iterable objects such as string, array, map, set etc.
-const students = ["s1", "s2", "s3", "s4"];
-for(let name of students){
+const students1 = ["s1", "s2", "s3", "s4"];
+for(let name of students1){
     console.log(name)
 }
 
@@ -13,4 +13,14 @@ cgpa : 3.75
 for(let x in students2){ 
    console.log(x); //printing all the keys
     console.log(students2[x]); //printing all the values
+}
+
+let students = {
+    ID : 101,
+    name : 'Anisul Islam',
+    cgpa : 3.91
+}
+
+for(let x in students){
+    console.log(`${x} : ${students[x]}`)
 }
