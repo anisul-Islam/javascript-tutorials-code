43d31299b4eb3139de3837c4a2c0241e88bf1564
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-25 15:37:19 +0200

    es6 array methods

--- ES6 FEATURES/4_ARRAY_METHODS/index.js
+++ ES6 FEATURES/4_ARRAY_METHODS/index.js
@@ -1,40 +1,37 @@
+// Array methods
 // find() returns the value of the first array element that passes certain condition
-let numbers = [5,55,30,40,50];
+let numbers = [5, 55, 30, 40, 50];
 
+const evenNumbers = (value, index, array) => {
+  if (value % 2 === 0) {
+    return value;
+  }
+};
 
-const evenNumbers = (value, index, array) =>{
-    if(value%2===0){
-        return value;
-    }
-}
-
-let newNumbers = numbers.find(evenNumbers)
+let newNumbers = numbers.find(evenNumbers);
 // let newNumbers = numbers.find(x => x>25)
-console.log(newNumbers)
+console.log(newNumbers);
 
 // findIndex() returns the index of the first array element that passes certain condition
-console.log(numbers.findIndex(evenNumbers))
+console.log(numbers.findIndex(evenNumbers));
 
 const students = [
-    {
-        id : 101,
-        gpa : 3.5
-    },
-    {
-        id : 102,
-        gpa : 2
-    },
-    {
-        id : 103,
-        gpa : 4.5
-    },
-    {
-        id : 104,
-        gpa : 5
-    },
-]
+  {
+    id: 101,
+    gpa: 3.5,
+  },
+  {
+    id: 102,
+    gpa: 2,
+  },
+  {
+    id: 103,
+    gpa: 4.5,
+  },
+  {
+    id: 104,
+    gpa: 5,
+  },
+];
 
-console.log(students.find(x => x.gpa > 4))
-
-
-
+console.log(students.find((x) => x.gpa > 4));
