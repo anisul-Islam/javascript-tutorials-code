f1865efef842c3bfc0d2829a0b1607a4351703a3
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-25 15:37:56 +0200

    es6 string  methods

--- ES6 FEATURES/5_STRING_METHODS/index.js
+++ ES6 FEATURES/5_STRING_METHODS/index.js
@@ -1,18 +1,19 @@
+// es6 string methods
 // startsWith(searchString, position) -> check a string starts with another string
 // all these methods are case sensitive
 
-const message = "Today is friday"
+const message = "Today is friday";
 
-console.log(message.startsWith('Today'))
-console.log(message.startsWith('Today', 0))
-console.log(message.startsWith('Today', 5))
-console.log(message.startsWith('today'))
+console.log(message.startsWith("Today"));
+console.log(message.startsWith("Today", 0));
+console.log(message.startsWith("Today", 5));
+console.log(message.startsWith("today"));
 
 // endsWith(searchString, length) -> check a string ends with another string
-console.log(message.endsWith('today'))
-console.log(message.endsWith('friday'))
+console.log(message.endsWith("today"));
+console.log(message.endsWith("friday"));
 
 // includes(searchString, position) -> check if a string contains another string
-console.log(message.includes('Today'))
-console.log(message.includes('is'))
-console.log(message.includes('that'))
+console.log(message.includes("Today"));
+console.log(message.includes("is"));
+console.log(message.includes("that"));
