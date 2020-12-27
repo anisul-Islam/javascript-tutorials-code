a15cd8a35b4903df061f13b31f522139ebd25daf
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-27 21:36:31 +0200

    synchronous vs asynchronous programming

---
+++ ES6 FEATURES/8_ASYNCHRONOUS_PROG/index1.js
@@ -0,0 +1,34 @@
+// sync vs async programming
+
+// example of synchronous js
+// js is single threaded, synchronous programming language.
+// it has blocking behaviour
+/*
+console.log("wake up");
+console.log("Get fresh");
+console.log("start breakfast");
+console.log("finish breakfast");
+console.log("Read Newspaper");
+console.log("Check social media");
+
+*/
+// example of non blocking js using setTimeout() asynchronous function
+// tasks will be added in call stack, when one task is done then it will moved to the next one (LIFO)
+// setTimeout() will move the task into web APIs where the task will be running in background
+// other task will continue in call stack
+// when the call stack is completely empty then task (callback functions) from web APIs will be moved to call-queue (FIFO)
+/*
+console.log("wake up");
+console.log("Get fresh");
+console.log("start breakfast");
+
+setTimeout(() => {
+  console.log("finish breakfast1");
+}, 2000);
+setTimeout(() => {
+  console.log("finish breakfast2");
+}, 2000);
+console.log("Read Newspaper");
+console.log("Check social media");
+
+*/
