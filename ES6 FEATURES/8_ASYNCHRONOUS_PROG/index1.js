afbdf2867fd993d175c7b8af72197280a2ddafb3
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-28 01:52:14 +0200

    synchrounous vs asynchronous

--- ES6 FEATURES/8_ASYNCHRONOUS_PROG/index1.js
+++ ES6 FEATURES/8_ASYNCHRONOUS_PROG/index1.js
@@ -1,34 +1,45 @@
-// sync vs async programming
+// sync vs async
 
 // example of synchronous js
 // js is single threaded, synchronous programming language.
 // it has blocking behaviour
-/*
-console.log("wake up");
-console.log("Get fresh");
-console.log("start breakfast");
-console.log("finish breakfast");
-console.log("Read Newspaper");
-console.log("Check social media");
 
-*/
+// console.log(`Task 1`);
+// console.log(`Task 2`);
+// console.log(`Task 3`);
+// console.log(`Task 4`);
+
 // example of non blocking js using setTimeout() asynchronous function
 // tasks will be added in call stack, when one task is done then it will moved to the next one (LIFO)
 // setTimeout() will move the task into web APIs where the task will be running in background
 // other task will continue in call stack
 // when the call stack is completely empty then task (callback functions) from web APIs will be moved to call-queue (FIFO)
-/*
-console.log("wake up");
-console.log("Get fresh");
-console.log("start breakfast");
 
-setTimeout(() => {
-  console.log("finish breakfast1");
-}, 2000);
-setTimeout(() => {
-  console.log("finish breakfast2");
-}, 2000);
-console.log("Read Newspaper");
-console.log("Check social media");
+// console.log(`Task 1`);
+// setTimeout(() => {
+//   console.log(`Task 2`);
+// }, 2000);
+// console.log(`Task 3`);
+// console.log(`Task 4`);
 
-*/
+const taskOne = () => {
+  console.log(`Task 1`);
+};
+
+const taskTwo = () => {
+  setTimeout(() => {
+    console.log(`Task 2`);
+  }, 2000);
+};
+
+const taskThree = () => {
+  console.log(`Task 3`);
+};
+const taskFour = () => {
+  console.log(`Task 4`);
+};
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
