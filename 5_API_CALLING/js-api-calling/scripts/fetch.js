3a73114c849dfddbf4b5ab86909803ce7f67d007
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-23 22:25:51 +0300

    fetch api

---
+++ 5_API_CALLING/js-api-calling/scripts/fetch.js
@@ -0,0 +1,72 @@
+// 4 ways to call api - XMLHttpRequest, fetch, axios, jquery
+
+// fetch() has replaced XMLHttpRequest
+// fetch() - global method for making HTTP Request
+// 2 ways to call - then, async await
+
+// + fetch() is easy to use compare to XMLHttpRequest
+// + fetch() returns a promise
+// - returned promise can only handle network error
+// - does not support all the older browser
+
+const makeRequest = async (url, config) => {
+  const res = await fetch(url, config);
+  if (!res.ok) {
+    const message = `Error : ${res.status}`;
+    throw new Error(message);
+  }
+  const data = await res.json();
+  return data;
+};
+
+const deleteData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+deleteData();
+
+// const updateData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+//     method: "PATCH",
+//     body: JSON.stringify({
+//       title: "foomaraarrara",
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// updateData();
+
+// const sendData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// sendData();
+
+// const getData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// getData();
