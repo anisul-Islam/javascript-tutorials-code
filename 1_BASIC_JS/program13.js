35b5da8145fa72cbf90f747e50e21c42d988e780
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:20:54 +0300

    switch statement - digit spelling program

--- 1_BASIC_JS/program13.js
+++ 1_BASIC_JS/program13.js
@@ -5,50 +5,50 @@
 // ...
 // ...
 // 9 -> Nine
-// for any number program will display "not a digit"
+// if input is not a number then program will display "not a digit"
 
 var digit = parseInt(prompt("Enter a digit : "));
 switch (digit) {
-    case 0:
-        document.write("Zero");
-        break;
+  case 0:
+    document.write("Zero");
+    break;
 
-    case 1:
-        document.write("One");
-        break;
+  case 1:
+    document.write("One");
+    break;
 
-    case 2:
-        document.write("Two");
-        break;
+  case 2:
+    document.write("Two");
+    break;
 
-    case 3:
-        document.write("Three");
-        break;
+  case 3:
+    document.write("Three");
+    break;
 
-    case 4:
-        document.write("Four");
-        break;
+  case 4:
+    document.write("Four");
+    break;
 
-    case 5:
-        document.write("Five");
-        break;
+  case 5:
+    document.write("Five");
+    break;
 
-    case 6:
-        document.write("Six");
-        break;
+  case 6:
+    document.write("Six");
+    break;
 
-    case 7:
-        document.write("Seven");
-        break;
+  case 7:
+    document.write("Seven");
+    break;
 
-    case 8:
-        document.write("Eight");
-        break;
+  case 8:
+    document.write("Eight");
+    break;
 
-    case 9:
-        document.write("Nine");
-        break;
+  case 9:
+    document.write("Nine");
+    break;
 
-    default:
-        document.write("Not a digit");
+  default:
+    document.write("Not a digit");
 }
