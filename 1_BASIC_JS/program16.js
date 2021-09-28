b3059c580d401d2ede424f7031f625a6517a1c38
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:23:05 +0300

    do while loop - sum of numbers

--- 1_BASIC_JS/program16.js
+++ 1_BASIC_JS/program16.js
@@ -2,18 +2,17 @@
 // do while loop syntax
 var i = 1;
 do {
-    document.write("Bangladesh" + "<br>");
-    i++;
+  document.write("Bangladesh" + "<br>");
+  i++;
 } while (i <= 10);
 
-document.write("End of for loop");
-
+document.write("End of do while loop");
 
 // sum of numbers 1+2+..+100
-// var sum = 0;
-// var x = 1;
-// do{
-//     sum = sum + x;
-//     x++;
-// }while ( x <= 100)
-// document.write(sum);
+var sum = 0;
+var x = 1;
+do {
+  sum = sum + x;
+  x++;
+} while (x <= 100);
+document.write(sum);
