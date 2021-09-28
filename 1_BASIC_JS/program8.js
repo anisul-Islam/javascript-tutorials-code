6815e4ab1220d27fad9273e5c857e5bde4281ce8
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:15:24 +0300

    Area Calculator

--- 1_BASIC_JS/program8.js
+++ 1_BASIC_JS/program8.js
@@ -1,13 +1,13 @@
 // Area of various shapes
-
 //Area of triangel
+
 //getting user input for base
 var base = parseFloat(prompt("Enter Base = "));
 
 //getting user input for height
 var height = parseFloat(prompt("Enter Height = "));
 
-//calculating area 
+//calculating area
 var area = 0.5 * base * height;
 
 //printing area
