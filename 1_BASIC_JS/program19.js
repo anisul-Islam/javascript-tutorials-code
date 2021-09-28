700bfe05826d8eb580e52250f53c43fad3a6fdeb
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 20:31:37 +0300

    IIFEs & function expression

---
+++ 1_BASIC_JS/program19.js
@@ -0,0 +1,14 @@
+// IIFEs (Immediately Invokeable Function Expressions)
+
+// (function display(message) {
+//   console.log(message);
+// })('hi');
+
+// Function Expressions
+var display2 = function displayMessage(msg) {
+  console.log(msg);
+};
+display2("I am message");
+
+// ( ), { }, [ ], < >
+// Task 7: create an IIFEs that print sum of 2 numbers
