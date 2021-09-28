d06bf22300bb9dfd7403e0aa6ccd383836036d90
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 22:23:51 +0300

    Sorting

--- 1_BASIC_JS/program23.js
+++ 1_BASIC_JS/program23.js
@@ -1,31 +1,28 @@
 //sorting program
-// The sort() method sorts an array alphabetically: 
-var fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.sort(); // Sorts the elements of fruits 
 
+// The sort() method sorts an array alphabetically:
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort(); // Sorts the elements of fruits
 
 //The reverse() method reverses the elements in an array.
-// You can use it to sort an array in descending order: 
+// You can use it to sort an array in descending order:
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.sort(); // First sort the elements of fruits
-fruits.reverse(); // Then reverse the order of the elements 
-
+fruits.reverse(); // Then reverse the order of the elements
 
 // You can fix this by providing a compare function:
 var points = [40, 100, 1, 5, 25, 10];
 points.sort(function (a, b) {
-    console.log(a + " , " + b);
-    console.log(points);
-    return a - b;
+  console.log(a + " , " + b);
+  console.log(points);
+  return a - b;
 });
-
 
 // se the same trick to sort an array descending:
 var points = [40, 100, 1, 5, 25, 10];
 points.sort(function (a, b) {
-    return b - a;
+  return b - a;
 });
-
 
 /*
 The Compare Function
