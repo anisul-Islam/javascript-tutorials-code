810b035b67ab2ff3e86e5845bde281c2ec6bd234
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:03:09 +0300

    how to print in js

--- 1_BASIC_JS/program1.js
+++ 1_BASIC_JS/program1.js
@@ -1,13 +1,12 @@
 // output functions -> functions that will help you to show output
 // document.write(), console.log(), alert()
-
+// console.clear(), console.error(), console.info(), console.warn()
 
 document.write("Welcome to JS Program");
 document.write("<h1>Welcome to JS Program</h1>");
 document.write("<h2>Welcome to JS Program</h2>");
 
-
 console.log("Welcome to JS Program");
 
-//this alert() will display an alert message 
+//this alert() will display an alert message
 alert("welcome to js program");
