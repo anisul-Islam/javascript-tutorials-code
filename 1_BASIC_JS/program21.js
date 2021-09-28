afb4c6091f38d40956256b6a5e42b05e2946349e
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 22:22:50 +0300

    Loop in one dimensional array

--- 1_BASIC_JS/program21.js
+++ 1_BASIC_JS/program21.js
@@ -1,7 +1,7 @@
-// Loop in array program
+// Loop in one dimensional array program
 
 // declaring an array that has 5 elements
-var num = [10, 20, 30, 40, 50]
+var num = [10, 20, 30, 40, 50];
 
 // we can print like following
 // console.log(num[0]);
@@ -10,10 +10,9 @@ var num = [10, 20, 30, 40, 50]
 // console.log(num[3]);
 // console.log(num[4]);
 
-
 // but we will use for loop to do this task more easily
 for (var i = 0; i < num.length; i++) {
-    console.log(num[i]);
+  console.log(num[i]);
 }
 
 // taking  numbers from user using an array and finding the sum
@@ -21,9 +20,9 @@ var numbers = [];
 var sum = 0;
 var n = parseInt(prompt("how many numbers you want to take? "));
 for (var x = 0; x < n; x++) {
-    numbers[x] = parseInt(prompt("Enter number : "));
-    // numbers.push(parseInt(prompt("Enter number : ")));
-    sum = sum + numbers[x];
+  numbers[x] = parseInt(prompt("Enter number : "));
+  // numbers.push(parseInt(prompt("Enter number : ")));
+  sum = sum + numbers[x];
 }
 console.log(numbers);
 console.log("sum is : " + sum);
