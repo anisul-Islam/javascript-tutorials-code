3bbc2c58eadaed1d1a4ef65b40a5620b5062c03f
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:16:50 +0300

    Relational & Logical Operator

--- 1_BASIC_JS/program10.js
+++ 1_BASIC_JS/program10.js
@@ -10,18 +10,17 @@ var num3 = "10";
 var num4 = 20;
 var num5 = 15;
 
-console.log(num1 > num2)
-console.log(num1 >= num2)
-console.log(num1 < num2)
-console.log(num1 <= num2)
-console.log(num1 == num4)
-console.log(num1 != num4)
-console.log(num1 === num3)
-console.log("num2 === num3 : "+ (num2 === num3))
-console.log("num2 == num3 : "+ (num2 == num3))
-console.log(num1 !== num3)
-
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+console.log(num1 == num4);
+console.log(num1 != num4);
+console.log(num1 === num3);
+console.log("num2 === num3 : " + (num2 === num3));
+console.log("num2 == num3 : " + (num2 == num3));
+console.log(num1 !== num3);
 
 // logical operators helps us to compine multiple conditions
-console.log(num1 > num2 && num1 > num5)
-console.log(num1 > num2 || num1 > num5)
+console.log(num1 > num2 && num1 > num5);
+console.log(num1 > num2 || num1 > num5);
