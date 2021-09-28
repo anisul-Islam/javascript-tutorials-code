fe7e47bec855e36be2507cb7398d8ab6bb64f63c
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 23:24:25 +0300

    json data

---
+++ 8_JSON/index.js
@@ -0,0 +1,17 @@
+const data = require("./friends.json");
+console.clear();
+
+// Accessing JSON Data
+// console.log(data);
+// console.log(data.friends[0]);
+// console.log(data.friends[0].name);
+
+// Modify JSON Data
+// data.friends[0].age = 35;
+// console.log(data.friends[0]);
+
+// delete JSON data
+// delete data.friends[0].age;
+// console.log(data.friends[0]);
+
+// conversion between js and json
