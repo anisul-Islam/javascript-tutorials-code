b9cd2d69a73166a4afd7c0662416c837235a7b8c
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 22:27:33 +0300

    map and filter array function

--- 1_BASIC_JS/program38.js
+++ 1_BASIC_JS/program38.js
@@ -1,14 +1,13 @@
-// forEach , map, filter
+// map, filter
 
-var numbers = [22,31,4,5,35,26,78]
-var squareNumbers = numbers.map(function(x){
-   return x*x;
-})
-console.log(squareNumbers)
+var numbers = [22, 31, 4, 5, 35, 26, 78];
+var squareNumbers = numbers.map(function (x) {
+  return x * x;
+});
+console.log(squareNumbers);
 
-
-var numbers = [22,31,4,5,35,26,78]
-var newNumbers = numbers.filter(function(x){
-   return x>10;
-})
-console.log(newNumbers)
+var numbers = [22, 31, 4, 5, 35, 26, 78];
+var newNumbers = numbers.filter(function (x) {
+  return x > 10;
+});
+console.log(newNumbers);
