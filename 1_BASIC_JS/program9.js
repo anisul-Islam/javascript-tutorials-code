24b11817b6bcfa68fb1fca22ba0840f8378fcbb9
Anisul_Islam <anisul_islam@AnisulIslamsMBP.home>
2021-09-28 19:15:55 +0300

    Temperature Converter

--- 1_BASIC_JS/program9.js
+++ 1_BASIC_JS/program9.js
@@ -4,7 +4,6 @@ var farn = parseFloat(prompt("Enter Fahrenheit = "));
 var cels = (farn - 32) * (5 / 9);
 document.write("Celsius = " + cels + "<br/>");
 
-
 // var cels = parseFloat(prompt("Enter Celsius = "));
 // var farn = (cels * (9 / 5)) + 32;
 // document.write("Fahrenheit = " + farn);
