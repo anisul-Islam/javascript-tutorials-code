cf4e9e4544a11029739634d822167743b0e8053d
anisul-islam <anisul2010s@yahoo.co.uk>
2021-10-06 20:19:04 +0300

    2d array

--- 2_DATA_STRUCTURE/index5.js
+++ 2_DATA_STRUCTURE/index5.js
@@ -4,21 +4,25 @@
 // Based on highest score, return the name of the player
 
 function highestRunScorer(playersInfo) {
-  let highestScorer = playersInfo[0][0];
-  let highestScore = playersInfo[0][1];
+  var highestScorer = playersInfo[0][0];
+  var highestScore = playersInfo[0][1];
 
   for (var x = 1; x < playersInfo.length; x++) {
     if (highestScore < playersInfo[x][1]) {
+      highestScore = playersInfo[x][1];
       highestScorer = playersInfo[x][0];
     }
   }
+
   return highestScorer;
 }
+
 var playersInfo = [
   ["Ashraful", 95],
-  ["Mizan", 25],
+  ["Mizan", 125],
   ["Rakib", 45],
   ["lu", 105],
 ];
-let x = highestRunScorer(playersInfo);
-console.log(x);
+
+var name = highestRunScorer(playersInfo);
+console.log(name);
